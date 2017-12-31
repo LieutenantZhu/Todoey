8ee15511a2ade1bba35be1edb7a3f931f305925a
朱冬 <xiefangfang@xiefangfangs-macbook-air.local>
2017-12-31 17:57:55 +0100

    Core data ready with categories

--- Todo/Controller/ToDoListViewController.swift
+++ Todo/Controller/ToDoListViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewCOntroller: UITableViewController {
 
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var selectedCategory : Category?{
+        didSet{
+            // when set the value, then trigger the action here
+             loadItems()
+        }
+    }
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loadItems()
-
+        searchBar.delegate = self
+       
     }
 
-    //MARK - Tableview Datasource Method
+//MARK - Tableview Datasource Method
     
     // Input table then select number of views in section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,27 +46,29 @@ class ToDoListViewCOntroller: UITableViewController {
         
         cell.textLabel?.text = item.title
         // now change to some new class
-        
         // we will try to use Yernary operator, value condition
         // cell.accessoryType = item.done == true ? .checkmark :.none
         
         cell.accessoryType = item.done ? .checkmark: .none
-        
+    
         return cell
     }
     
-    //MARK - Tableview Delegate Method
+//MARK - Tableview Delegate Method
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+//        first delete from context, then from array, order of the code are important, cause IndexPath already changed
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //MARK - Add New Items
+//MARK - Add New Items
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -68,53 +78,85 @@ class ToDoListViewCOntroller: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
-            
-            let newItem = Item()
+            //Can not be used as: context = AppDelegate.persistentCOntainer.viewCont, cause AppDelegate is only a class(Blue Print), this time we use singleton
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.parentCategory = self.selectedCategory
+            newItem.done = false
             self.itemArray.append(newItem)
             // save data to userdefault, store in PL list, need key
-            
             self.saveItems()
-            
-            
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Creat New Item"
             textField = alertTextField
-    
         }
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    // MARK - Model Manipulation Method
+    
+// MARK - Model Manipulation Method
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
-        
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch{
-            print("Error encoding itemArray, \(error)")
+            print("Error saving context, \(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)}
-                // we can not specify type of the data so use .self
-            catch{
-                print("Item decoding Error,\(error)")
-            }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(),with predicate: NSPredicate? = nil){
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else{
+            request.predicate = categoryPredicate
         }
+        
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate])
+//        request.predicate = compoundPredicate
+        
+        do{
+         itemArray =  try context.fetch(request)
+        }catch{
+            print("Error in reading message\(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK: - search bar method
+
+extension ToDoListViewCOntroller: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+//        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+//        request.predicate = predicate
+//        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+//        request.sortDescriptors = [sortDescriptor]
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, with: predicate)
         
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+// Back to the main thread with priority
+        }
+    }
 }
 
